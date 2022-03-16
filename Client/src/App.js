712c64f94d1cff136569c523f2c85dd8303c16e3
NaifAlzrigat <naif.alzrigat@gmail.com>
2022-03-17 01:00:00 +0300

    footer componnent

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,15 +1,12 @@
+import Footer from "./components/Footer/footer";
 import Header from "./components/Header/header";
 
 function App() {
   return (
     <div className="layout">
-      <Header/>
-      <main>
-        Content
-      </main>
-      <footer>
-        Footer
-      </footer>
+      <Header />
+      <main>Content</main>
+      <Footer />
     </div>
   );
 }
---
+++ Client/src/components/Footer/footer.js
@@ -0,0 +1,5 @@
+import React from "react";
+import "../../css/Footer/footer.css";
+export default function Footer() {
+  return <footer>&copy; Copy Right For Naif Alzrigat 2022 </footer>;
+}
