7f6b53e3ff14b984b055602203de3c223bc05df2
NaifAlzrigat <naif.alzrigat@gmail.com>
2022-03-17 23:34:59 +0300

    static words file

--- Client/src/components/Header/header.js
+++ Client/src/components/Header/header.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "../../css/Header/header.css";
+import { words } from "../../word";
 export default function Header() {
-  return <header>Shopping Cart</header>;
+  return <header>{words.headerTitle}</header>;
 }
